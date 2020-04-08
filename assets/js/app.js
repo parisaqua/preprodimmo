e68de2d95a3c323441fe375af993c4771bb6a1dd
Fred <frederic@pmo.digital>
2020-04-08 18:00:29 +0200

    Documents ok mais sans upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -112,28 +112,28 @@ $annulButton.click(e => {
 })
 
 
-// Suppression des éléments
-// document.querySelectorAll('[data-delete]').forEach(a => {
-//   a.addEventListener('click', e => {
-//     e.preventDefault()
-//     fetch(a.getAttribute('href'), {
-//       method: 'DELETE',
-//       headers: {
-//         'X-Requested-With': 'XMLHttpRequest',
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({'_token': a.dataset.token})
-//     }).then(response => response.json())
-//       .then(data => {
-//         if (data.success) {
-//           a.parentNode.parentNode.removeChild(a.parentNode)
-//         } else {
-//           alert(data.error)
-//         }
-//       })
-//       .catch(e => alert(e))
-//   })
-// })
+// Suppression des photos
+document.querySelectorAll('[data-delete]').forEach(a => {
+  a.addEventListener('click', e => {
+    e.preventDefault()
+    fetch(a.getAttribute('href'), {
+      method: 'DELETE',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({'_token': a.dataset.token})
+    }).then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          a.parentNode.parentNode.removeChild(a.parentNode)
+        } else {
+          alert(data.error)
+        }
+      })
+      .catch(e => alert(e))
+  })
+})
 
 //Image preview onload inputAddress
 
